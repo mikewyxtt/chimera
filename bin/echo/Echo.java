799e46b259f377fd7abb091e64c23902eae8f3a5
Michael Wyatt <michaelwyatt@Michaels-MacBook-Pro.local>
2023-03-26 17:11:04 -0400

    Cleaning up stuff...

--- bin/echo/Echo.java
+++ bin/echo/Echo.java
@@ -3,11 +3,11 @@
  * 
  * Copyright (C) 2023 Michael Wyatt
  * 
- * This file is part of NORDIX.
- * NORDIX is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation,
- * either version 3 of the License, or (at your option) any later version. NORDIX is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * This file is part of Chimera.
+ * Chimera is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation,
+ * either version 3 of the License, or (at your option) any later version. Chimera is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. You should have received a
- * copy of the GNU General Public License along with NORDIX. If not, see <https://www.gnu.org/licenses/>.
+ * copy of the GNU General Public License along with Chimera. If not, see <https://www.gnu.org/licenses/>.
  * 
  */
 

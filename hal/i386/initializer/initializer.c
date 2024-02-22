d9e75890a11d9038251922ae99b753e0e209bfe1
Mike Wyatt <wyattmich61@gmail.com>
2024-02-22 03:11:27 -0500

    basic serial logging setup

--- hal/i386/initializer/initializer.c
+++ hal/i386/initializer/initializer.c
@@ -1,5 +1,38 @@
+#include "multiboot.h"
+#include <stdint.h>
 
-void main() {
-    asm("mov eax, 0xfff");
+void log(char* msg);
+void serial_putc(char c);
+void outb(uint16_t port, uint8_t data);
+int strlen(char* str);
+
+void main(uint32_t magic, uint32_t multiboot_header_addr) {
+    log("asdf");
     while(1);
+}
+
+
+void log(char* msg) {
+    for(int i = 0; msg[i] != '\0'; i++) {
+        serial_putc(msg[i]);
+    }
+
+    serial_putc('\n');
+    serial_putc('\r');
+}
+
+void outb(uint16_t port, uint8_t data) {
+    asm volatile ("out %1, %0" : : "a" (data), "d" (port));
+}
+
+void serial_putc(char c) {
+    outb(0x3f8, c);
+}
+
+int strlen(char* str) {
+    int length = 0;
+    while(*str++) {
+        length++;
+    }
+    return length;
 }

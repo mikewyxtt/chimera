23823de6dc13858c363dd5a6b8df4169c58810de
Mike Wyatt <wyattmich61@gmail.com>
2024-02-22 20:20:39 -0500

    updated to conform with sys v abi

--- hal/i386/initializer/start.asm
+++ hal/i386/initializer/start.asm
@@ -5,18 +5,19 @@
 SECTION .multiboot
 ALIGN 8
 ;; Multiboot2 Header
-DD 0xE85250D6                           ; Magic Number
-DD 0x00                                 ; Architecture (0 is 32 bit protected mode)
-DD 0x10                                 ; Header length (16 bytes)
-DD -(0xE85250D6 + 0x00 + 0x10)          ; Checksum
+DD  0xE85250D6                           ; Magic Number
+DD  0x00                                 ; Architecture (0 is 32 bit protected mode)
+DD  0x10                                 ; Header length (16 bytes)
+DD  -(0xE85250D6 + 0x00 + 0x10)          ; Checksum
 
 ;; Multiboot tags
 ;; Multiboot information request
 DW  0x01                                ; Tell GRUB we want to request info
 DW  0x00                                ; Flags
 DD  0x20                                ; Size
-DD  0x01, 0x03, 0x04, 0x06, 0x08, 0x00  ; Multiboot information request:
-                                        ;  1: boot command line, 3: module info, 4: basic memory information, 6: memory map, 8: fb info, (padding for 8byte align)
+DD  0x01, 0x03, 0x04, 0x06, 0x08        ; Multiboot information request:
+                                        ;  1: boot command line, 3: module info, 4: basic memory information, 6: memory map, 8: fb info
+DD  0x00                                 ; Padding (Everything myst be 8 byte aligned)
 
 ;; Request a framebuffer
 DW  0x05                                ; Tag type. The presence of this tag tells GRUB we have framebuffer support
@@ -38,10 +39,13 @@ GLOBAL _start
 _start:
     CLI                                 ; Disable Interupts
 
-    CMP     EAX, 0x36D76289             ; Verify Multiboot2 magic number is present from bootloader
-    JNE     .loop                       ; Hang if magic number not equal. We should probably present some kind of error code somehow...
-
     MOV     ESP, stack_bottom + 0x4000  ; 16KB stack
+
+    push 0                              ; Reset EFLAGS register
+    popf                                ; ^^
+
+    PUSH EBX                            ; Pass multiboot header address to main()
+    PUSH EAX                            ; Pass magic number to main()
     CALL    main
 
 .loop:                                  ; Infinite loop
@@ -49,4 +53,4 @@ _start:
     JMP     .loop
 
 SECTION .bss
-stack_bottom    resb    0x4000          ; 16KB stack
+stack_bottom    RESB    0x4000          ; 16KB stack
